94cf4e228e81ed309522621486d8990ba7c77376
iskyd <careddumattia@gmail.com>
2021-12-11 18:09:12 +0100

    explicit count for true

--- 2021/day1/main.jl
+++ 2021/day1/main.jl
@@ -2,7 +2,7 @@ lines = readlines("input.txt")
 lines = [parse(Int16, line) for line in lines]
 
 function count_part_1(lines)
-	return count(i -> (i == 1), [lines[i] > lines[i - 1] ? true : false for i in 2:size(lines)[1]])
+	return count(i -> (i == true), [lines[i] > lines[i - 1] ? true : false for i in 2:size(lines)[1]])
 end
 
 function count_part_2(lines)
