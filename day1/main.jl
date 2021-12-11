b6331676b268597993c317916701b5274e4fb152
iskyd <careddumattia@gmail.com>
2021-12-11 18:07:24 +0100

    refactor and part 2

---
+++ day1/main.jl
@@ -0,0 +1,13 @@
+lines = readlines("input.txt")
+lines = [parse(Int16, line) for line in lines]
+
+function count_part_1(lines)
+	return count(i -> (i == 1), [lines[i] > lines[i - 1] ? true : false for i in 2:size(lines)[1]])
+end
+
+function count_part_2(lines)
+	return count_part_1([lines[i] + lines[i - 1] + lines[i - 2] for i in 3:size(lines)[1]])
+end
+
+println("Solution part 1: ", count_part_1(lines))
+println("Solution part 2: ", count_part_2(lines))
--- main.jl
+++
@@ -1,7 +0,0 @@
-lines = readlines("input.txt")
-
-function count_list_comprehensions(lines)
-	return count(i -> (i == 1), [parse(Int64, lines[i]) > parse(Int64, lines[i - 1]) ? true : false for i in 2:size(lines)[1]])
-end
-
-print(count_list_comprehensions(lines))
