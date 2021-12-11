28410b8704c5f834c73215d9d02902c96ac994ad
iskyd <careddumattia@gmail.com>
2021-12-11 18:32:21 +0100

    day2

---
+++ 2021/day2/main.jl
@@ -0,0 +1,43 @@
+lines = readlines("input.txt")
+
+
+function solution_part_1(lines)
+    depth = 0
+    horizontal = 0
+    for line in lines
+        action, value = split(line, " ")
+        value = parse(Int16, value)
+        if action == "up"
+            depth -= value
+        elseif action == "down"
+            depth += value
+        elseif action == "forward"
+            horizontal += value
+        end
+    end
+
+    return horizontal * depth
+end
+
+function solution_part_2(lines)
+    depth = 0
+    horizontal = 0
+    aim = 0
+    for line in lines
+        action, value = split(line, " ")
+        value = parse(Int16, value)
+        if action == "up"
+            aim -= value
+        elseif action == "down"
+            aim += value
+        elseif action == "forward"
+            horizontal += value
+            depth += aim * value
+        end
+    end
+
+    return horizontal * depth
+end
+
+println(solution_part_1(lines))
+println(solution_part_2(lines))
