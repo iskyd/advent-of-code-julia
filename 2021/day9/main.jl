803cd8916b1a6efbe86935905879b7bcd821529b
iskyd <careddumattia@gmail.com>
2021-12-15 14:59:50 +0100

    day9 part 2

--- 2021/day9/main.jl
+++ 2021/day9/main.jl
@@ -39,6 +39,46 @@ function total_heigth_of_lowest_points(lowest_points)
     return sum(lowest_points + v)
 end
 
+function find_largest_basins(matrix, index_lowest_points)
+    basins = []
+    for index_lowest_point in index_lowest_points 
+        push!(basins, find_basin_size(matrix, index_lowest_point))
+    end
+
+    return sort(basins, rev=true)[1:3]
+end
+
+function get_neighbors_points(matrix, index)
+    adjacent_transormation = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    neighbors = [index + transformation for transformation in adjacent_transormation]
+
+    return neighbors
+end
+
+function find_basin_size(matrix, index_lowest_point)
+    padding_matrix = add_padding(matrix)
+    next_indexes = get_neighbors_points(padding_matrix, index_lowest_point + [1, 1])
+    valid_indexes = [index_lowest_point + [1, 1]]
+    total_size = 1
+    while !(isempty(next_indexes))
+        index = pop!(next_indexes)
+        if padding_matrix[index...] != 9
+            total_size += 1
+            push!(valid_indexes, index)
+            neighbors = get_neighbors_points(padding_matrix, index)
+            for neighbor in neighbors
+                if !(neighbor in next_indexes) && !(neighbor in valid_indexes) && padding_matrix[neighbor...] != 9
+                    push!(next_indexes, neighbor)
+                end
+            end
+        end
+    end
+
+    return total_size
+end
+
 matrix = create_matrix(lines)
 index_lowest_points = find_index_lowest_points(matrix)
 println("Solution part 1: ", total_heigth_of_lowest_points([matrix[x[1],x[2]] for x in index_lowest_points]))
+
+println("Solution part 2: ", prod(find_largest_basins(matrix, index_lowest_points)))
