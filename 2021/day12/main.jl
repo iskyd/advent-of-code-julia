f0ed139ca17a22ef0bb8303acc5ae091765a7a8a
iskyd <careddumattia@gmail.com>
2021-12-21 17:40:14 +0100

    day12 part 2

--- 2021/day12/main.jl
+++ 2021/day12/main.jl
@@ -36,5 +36,37 @@ function solution_part_1(nodes)
     return length(complete)
 end
 
+function solution_part_2(nodes)
+    incomplete = [["start"]]
+    complete = []
+
+    while length(incomplete) > 0
+        previous = pop!(incomplete)
+        d = Dict([(i, count(x -> x == i, previous)) for i in unique(previous)])
+        multiple_visits_occurred = false
+        for (key, value) in d
+            if nodes[key]["multiple_visits_allowed"] === false && value > 1
+                multiple_visits_occurred = true
+                break
+            end
+        end
+
+        for node in nodes[last(previous)]["connections"]
+            if nodes[node]["multiple_visits_allowed"] === false && multiple_visits_occurred === true && node in previous continue end
+            if node == "start" continue end
+            
+            tmp = copy(previous)
+            if node == "end"
+                push!(complete, push!(tmp, "end"))
+            else
+                push!(incomplete, push!(tmp, node))
+            end
+        end
+    end
+
+    return length(complete)
+end
+
 nodes = build_nodes(lines)
 println("Solution part 1: ", solution_part_1(nodes))
+println("Solution part 2: ", solution_part_2(nodes))
