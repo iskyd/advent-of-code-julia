745b572de8838726c9e7393cd5b04793f5f1c0fd
iskyd <careddumattia@gmail.com>
2022-01-03 14:33:45 +0100

    day 21 part 2

--- 2021/day21/main.jl
+++ 2021/day21/main.jl
@@ -1,4 +1,9 @@
+using IterTools
+using Caching
+
 lines = readlines("input.txt")
+
+BOARD_LENGTH = 10
 
 function init_game(lines)
     players = Dict(
@@ -12,13 +17,10 @@ function init_game(lines)
         )
     )
 
-    board = []
-
     return players
 end
 
 function solution_part_1(players)
-    board_length = 10
     last_dice_value = 0
     current_player = 1
     total_rolled = 0
@@ -30,7 +32,7 @@ function solution_part_1(players)
         last_dice_value = dice_values[end]
         
         new_position = players[current_player]["position"] + sum(dice_values)
-        new_position = new_position .% board_length
+        new_position = new_position .% BOARD_LENGTH
         if new_position == 0 new_position = 10 end
         
         players[current_player]["position"] = new_position
@@ -45,5 +47,41 @@ function solution_part_1(players)
     return players[loser]["score"] * total_rolled * 3
 end
 
+@cache function play_out(p1_position, p1_score, p2_position, p2_score, current_player)
+    if p1_score >= 21 return 1, 0 elseif p2_score >= 21 return 0, 1 end
+
+    tot_p1_wins = 0
+    tot_p2_wins = 0
+
+    for (d1, d2, d3) in product((1, 2, 3), (1, 2, 3), (1, 2, 3))
+        starting_position = current_player == 1 ? p1_position : p2_position
+
+        new_position = starting_position + (d1 + d2 + d3)
+        new_position = new_position .% BOARD_LENGTH
+        if new_position == 0 new_position = 10 end
+        
+        if current_player == 1
+            new_score = p1_score + new_position
+            p1_wins, p2_wins = play_out(new_position, new_score, p2_position, p2_score, 2)
+        else
+            new_score = p2_score + new_position
+            p1_wins, p2_wins = play_out(p1_position, p1_score, new_position, new_score, 1)
+        end
+
+        tot_p1_wins += p1_wins
+        tot_p2_wins += p2_wins
+    end
+
+    return tot_p1_wins, tot_p2_wins
+end
+
+function solution_part_2(players)
+    p1_wins, p2_wins = play_out(players[1]["position"], 0, players[2]["position"], 0, 1)
+    return max(p1_wins, p2_wins)
+end
+
 players = init_game(lines)
 println("Solution part 1: ", solution_part_1(players))
+
+players = init_game(lines)
+println("Solution part 2: ", solution_part_2(players))
