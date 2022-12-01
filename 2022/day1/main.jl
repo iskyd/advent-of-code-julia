0c82cb20cad0328c17cc73c8ba93fc838c9d5b35
iskyd <careddumattia@gmail.com>
2022-12-01 07:36:02 +0100

    finally its christmas 2022

---
+++ 2022/day1/main.jl
@@ -0,0 +1,40 @@
+function solutionPart1()::Int
+    maxElvesCalories = 0
+    currentElvesCalories = 0
+
+    for line in readlines("input.txt")
+        if line == ""
+            if currentElvesCalories > maxElvesCalories
+                maxElvesCalories = currentElvesCalories
+            end
+            currentElvesCalories = 0
+        else
+            currentElvesCalories += parse(Int, line)
+        end
+    end
+
+    return maxElvesCalories
+end
+
+function solutionPart2()::Int
+    # get top three elvesCalories
+    elvesCalories = Vector{Int}()
+    currentElvesCalories = 0
+    for line in readlines("input.txt")
+        if line == ""
+            push!(elvesCalories, currentElvesCalories)
+            elvesCalories = sort(elvesCalories, rev=true)
+            if length(elvesCalories) > 3
+                elvesCalories = elvesCalories[1:3]
+            end
+            currentElvesCalories = 0
+        else
+            currentElvesCalories += parse(Int, line)
+        end
+    end
+
+    return sum(elvesCalories)
+end
+
+println("Solution part 1: ", solutionPart1())
+println("Solution part 1: ", solutionPart2())
