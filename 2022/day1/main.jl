8738278b27f4751074bfa5bd7c90f76de543251d
Mattia <mattia.careddu@conio.com>
2022-12-01 10:28:01 +0100

    refactor day 1 using resumfunctions

--- 2022/day1/main.jl
+++ 2022/day1/main.jl
@@ -1,39 +1,31 @@
-function solutionPart1()::Int
-    maxElvesCalories = 0
-    currentElvesCalories = 0
+using ResumableFunctions
 
+@resumable function get_calories()
+    calory = 0
     for line in readlines("input.txt")
         if line == ""
-            if currentElvesCalories > maxElvesCalories
-                maxElvesCalories = currentElvesCalories
-            end
-            currentElvesCalories = 0
+            @yield calory
+            calory = 0
         else
-            currentElvesCalories += parse(Int, line)
+            calory += parse(Int, line)
         end
     end
-
-    return maxElvesCalories
 end
 
-function solutionPart2()::Int
+function solution()::Tuple{Int, Int}
     elvesCalories = Vector{Int}()
-    currentElvesCalories = 0
-    for line in readlines("input.txt")
-        if line == ""
-            push!(elvesCalories, currentElvesCalories)
-            elvesCalories = sort(elvesCalories, rev=true)
-            if length(elvesCalories) > 3
-                elvesCalories = elvesCalories[1:3]
-            end
-            currentElvesCalories = 0
-        else
-            currentElvesCalories += parse(Int, line)
+
+    for calory in get_calories()
+        push!(elvesCalories, calory)
+        elvesCalories = sort(elvesCalories, rev=true)
+        if length(elvesCalories) > 3
+            elvesCalories = elvesCalories[1:3]
         end
     end
 
-    return sum(elvesCalories)
+    return elvesCalories[1], sum(elvesCalories)
 end
 
-println("Solution part 1: ", solutionPart1())
-println("Solution part 2: ", solutionPart2())
+part1, part2 = solution()
+println("Solution part 1: ", part1)
+println("Solution part 2: ", part2)
