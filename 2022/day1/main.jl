c903002780ff66d15f541bd051239ef9565416cb
Mattia <mattia.careddu@conio.com>
2022-12-02 09:26:04 +0100

    optimized solution part 2 of day 1 without using array and sort/min functions

--- 2022/day1/main.jl
+++ 2022/day1/main.jl
@@ -2,7 +2,7 @@ using ResumableFunctions
 
 @resumable function get_calories()
     calory = 0
-    for line in readlines("input.txt")
+    for line in readlines("/home/mattia/dev/advent-of-code-julia/2022/day1/input.txt")
         if line == ""
             @yield calory
             calory = 0
@@ -26,6 +26,24 @@ function solution()::Tuple{Int, Int}
     return elvesCalories[1], sum(elvesCalories)
 end
 
+function optimized_part_2()::Int
+    total_calories, max, min = 0, 0, 0
+    for calory in get_calories()
+        if calory > min
+            total_calories = total_calories - min + calory
+            if calory >= max
+                min = total_calories - calory - max
+                max = calory
+            else
+                min = calory < total_calories - calory - max ? calory : (min === 0 ? calory : min)
+            end
+        end
+    end
+
+    return total_calories
+end
+
 part1, part2 = solution()
 println("Solution part 1: ", part1)
 println("Solution part 2: ", part2)
+println("Optimized part 2: ", optimized_part_2())
