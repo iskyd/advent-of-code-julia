f25c666a58cb31855f47fab0a1d5a2e806e27eb7
iskyd <careddumattia@gmail.com>
2022-12-06 07:41:53 +0100

    day6

---
+++ 2022/day6/main.jl
@@ -0,0 +1,22 @@
+function get_stream()::Vector{Char}
+    return map(c -> only(c), split(readlines("input.txt")[1], ""))
+end
+
+function solution(stream::Vector{Char}, n::Int)::Int
+    i = 1
+    while i < length(stream) - (n - 1)
+        if length(unique(stream[i:i + n - 1])) == n
+            break
+        end
+        i += 1
+    end
+
+    return i + n - 1
+end
+
+function solution_part2(stream::Vector{Char})::Int
+end
+
+stream = get_stream()
+println("Solution part 1: ", solution(stream, 4))
+println("Solution part 2: ", solution(stream, 14))
