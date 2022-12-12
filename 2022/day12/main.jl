23ee1c58f50cd39acf2e9392afd17175185054b8
iskyd <careddumattia@gmail.com>
2022-12-12 08:30:34 +0100

    day12 part 2

--- 2022/day12/main.jl
+++ 2022/day12/main.jl
@@ -1,13 +1,13 @@
 using DataStructures
 
-heightmap::Matrix{Int} = map(c -> Int(c), reduce(hcat, collect.(readlines("input.txt"))))
+heightmap::Matrix{Int} = map(c -> Int(c), reduce(hcat, collect.(readlines("test.txt"))))
 start_point = findfirst(x -> x == 83, heightmap::Matrix)
 end_point = findfirst(x -> x == 69, heightmap::Matrix)
 
 heightmap[start_point] = Int('a')
 heightmap[end_point] = Int('z')
 
-function solution_part1(heightmap::Matrix{Int}, start_point::CartesianIndex, end_point::CartesianIndex)::Int
+function get_min_steps(heightmap::Matrix{Int}, start_point::CartesianIndex, end_point::CartesianIndex, part2::Bool=false)::Int
     visited = Set{CartesianIndex}()
     queue = Queue{Tuple{Int, CartesianIndex}}()
     enqueue!(queue, (0, end_point))
@@ -16,8 +16,14 @@ function solution_part1(heightmap::Matrix{Int}, start_point::CartesianIndex, end
 
     while !isempty(queue)
         steps, current_point = dequeue!(queue)
-        if current_point == start_point
-            return steps
+        if part2
+            if heightmap[current_point] == Int('a')
+                return steps
+            end
+        else
+            if current_point == start_point
+                return steps
+            end
         end
 
         for delta in deltas
@@ -36,4 +42,5 @@ function solution_part1(heightmap::Matrix{Int}, start_point::CartesianIndex, end
     return -1
 end
 
-println("Solution part 1: ", solution_part1(heightmap, start_point, end_point))
+println("Solution part 1: ", get_min_steps(heightmap, start_point, end_point, false))
+println("Solution part 2: ", get_min_steps(heightmap, start_point, end_point, true))
